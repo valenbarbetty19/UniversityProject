ec21ee1b9947922b8dd8dff301d65f1a19a0f668
valenbarbetty19 <valentina.barbetty@globant.com>
2026-03-08 18:56:28 -0500

    Added course feature with constructor, and getters and stters

--- src/model/PartTimeTeacher.java
+++ src/model/PartTimeTeacher.java
@@ -1,4 +1,13 @@
 package model;
 
-public class PartTimeTeacher {
+public class PartTimeTeacher extends Teacher{
+    private double hoursPerWeek;
+    public PartTimeTeacher(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() * hoursPerWeek;
+    }
 }
