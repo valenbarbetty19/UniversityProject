1bbfb17736ff18820fe42d554f88bc54781e2528
valenbarbetty19 <valentina.barbetty@globant.com>
2026-03-09 09:59:58 -0500

    Created the menu with all the options to choose

---
+++ src/ui/Menu.java
@@ -0,0 +1,86 @@
+package ui;
+import model.Teacher;
+import service.UniversityService;
+import java.util.Scanner;
+
+public class Menu {
+
+    private UniversityService universityService;
+    private Scanner scanner;
+
+    public Menu() {
+        universityService = new UniversityService();
+        scanner = new Scanner(System.in);
+        universityService.initializeData();
+    }
+
+    public void start() {
+        int option;
+        do {
+            System.out.println("\n UNIVERSITY MENU ");
+            System.out.println("1. Print all teachers");
+            System.out.println("2. Print all courses");
+            System.out.println("3. Create new student");
+            System.out.println("4. Create new course");
+            System.out.println("5. List classes by student ID");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 1:
+                    universityService.printTeachers();
+                    break;
+                case 2:
+                    int courseOption = scanner.nextInt();
+                    universityService.printCourses(courseOption);
+                    break;
+                case 3:
+                    createStudentMenu();
+                    break;
+                case 4:
+                    createCourseMenu();
+                    break;
+                case 5:
+                    searchStudentCourses();
+                    break;
+                case 6:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        } while (option != 6);
+    }
+
+    private void createStudentMenu() {
+        System.out.print("Enter student ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter student name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter student age: ");
+        int age = scanner.nextInt();
+        System.out.print("Choose course option: ");
+        int courseOption = scanner.nextInt();
+        universityService.createStudent(id, name, age, courseOption);
+    }
+    private void createCourseMenu() {
+        scanner.nextLine();
+        System.out.print("Enter course name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter classroom: ");
+        String classroom = scanner.nextLine();
+        System.out.println("Choose teacher index:");
+        universityService.printTeachers();
+        int teacherOption = scanner.nextInt();
+        Teacher teacher = universityService.getUniversity().getTeachers().get(teacherOption);
+        universityService.createNewCourse(name, classroom, teacher);
+    }
+
+    private void searchStudentCourses() {
+        System.out.print("Enter student Id: ");
+        int id = scanner.nextInt();
+        universityService.listClassesByStudentId(id);
+    }
+}
